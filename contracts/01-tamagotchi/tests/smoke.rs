61e7d4dbb53a3df944e059d880740321c481e760
Diego Rosas <diego_f423@hotmail.com>
2024-01-24 20:03:25 +0000

    hw_01

--- contracts/01-tamagotchi/tests/smoke.rs
+++ contracts/01-tamagotchi/tests/smoke.rs
@@ -1,12 +1,21 @@
-use gtest::{Program, System};
+use gtest::{Program, System, Log};
+use tamagotchi_io::{TmgAction, TmgEvent};
 
 #[test]
 fn smoke_test() {
     let sys = System::new();
     sys.init_logger();
     let program = Program::current(&sys);
-
-    let init_name = "InitialTamagotchiName".to_string();
+    
+    let init_name = "Diego".to_string();
     let res_init = program.send(0, init_name.clone());
     assert!(!res_init.main_failed());
+    
+    
+    program.send(2, String::from("Diego".to_string()));
+    let res = program.send(2, TmgAction::Name);
+    let expected_log = Log::builder()
+    .dest(2)
+    .payload(TmgEvent::Name("Diego".to_string()));
+    assert!(res.contains(&expected_log));
 }
